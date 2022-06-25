997c5fb100424519a85b4ca69b1dcb46fc4a6b0c
Matias Blanc <programador.matiasblanc@gmail.com>
2022-06-25 16:19:34 -0400

    Hello World!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx';
-import Card from './components/Card.jsx';
+import Search from './components/Search.jsx';
 
 export default function App() {
 return (
 	<>
 		<Header />
-		<Card />
+		<main className="w-2/6 bg-white-200 rounded-2xl mx-auto my-2">
+		<Search />
+    	</main>
 	</>
 )
 }
